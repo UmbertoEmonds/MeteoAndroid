48cb164cab8bad35fa0a7f4ce1ba4fd3bf7841c1
Umberto Emonds <umberto.emonds@gmail.com>
2021-07-21 17:01:11 +0200

    Cocorico

--- app/src/main/java/fr/projet2/monprojetmeteo/activity/MainActivity.kt
+++ app/src/main/java/fr/projet2/monprojetmeteo/activity/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import fr.projet2.monprojetmeteo.R
 import fr.projet2.monprojetmeteo.model.Weathers
 import fr.projet2.monprojetmeteo.util.factory.ViewModelFactory
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mCityTV: TextView
     private lateinit var mCityET: EditText
     private lateinit var mDescriptionTV: TextView
+    private lateinit var mIconIV: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         mCityTV = findViewById(R.id.mCityTV)
         mCityET = findViewById(R.id.saisirET)
         mDescriptionTV = findViewById(R.id.mDescriptionTV)
+        mIconIV = findViewById(R.id.iconIV)
 
         configureViewModels()
     }
@@ -37,6 +41,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun updateUI(weathers: Weathers){
+
+        val iconUrl = "https://openweathermap.org/img/w/${weathers.list[0].weather[0].icon}.png"
+
+        Glide.with(applicationContext)
+            .load(iconUrl)
+            .into(mIconIV)
+
         mCityTV.text = weathers.list[0].name
         mDescriptionTV.text = "${weathers.list[0].weather[0].main} - ${weathers.list[0].weather[0].description}"
     }
